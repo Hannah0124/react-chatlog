51a50c774fac5520adf799ae577c9c9eb482b72b
Hannah <hannahjoo24@gmail.com>
2020-04-16 14:42:46 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
+import ChatHeader from './components/ChatHeader';
 
 const App = () => {
   console.log(chatMessages);
 
+  const senders = [];
+  chatMessages.forEach(message => {
+    if (!senders.includes(message.sender)) {
+      senders.push(message.sender)
+    }
+  })
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <ChatHeader senders={senders}/>
       </header>
       <main>
-
+        <ChatLog chatMessages={chatMessages}/>
       </main>
     </div>
   );
